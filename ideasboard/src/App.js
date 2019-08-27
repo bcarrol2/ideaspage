03815b6eeeefc8f2a28dff541a4a8953e7edfa8c
Blair <blairacarroll@gmail.com>
2019-08-27 19:35:53 -0400

    create-react-app

---
+++ ideasboard/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import IdeasContainer from './components/IdeasContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ideas Board</h1>
+      <IdeasContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ ideasboard/src/components/IdeasContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class IdeasContainer extends React.Component {
+    render(){
+        return(
+            <div>
+                <h2>Ideas</h2>
+            </div>
+        )
+    }
+}
