80f09c7e7d95682ba4b56559439dc17a518f8807
Blair <blairacarroll@gmail.com>
2019-08-27 20:04:18 -0400

    front ideasboard page created and connected with backend

--- ideasboard/src/components/IdeasContainer.js
+++ ideasboard/src/components/IdeasContainer.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class IdeasContainer extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            ideas: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3001/api/v1/ideas.json')
+        .then(response => {
+            console.log(response)
+            this.setState({ideas: response.data})
+        })
+        .catch(error => console.log(error))
+    }
+
     render(){
         return(
             <div>
-                <h2>Ideas</h2>
+                {this.state.ideas.map((idea) => {
+                        return (
+                            <div className="title" key={idea.id}>
+                                <h4>{idea.title}</h4>
+                                <p>{idea.body}</p>
+                            </div>
+                        )
+                    }
+                )}
             </div>
         )
     }
