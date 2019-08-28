76a551ba7c23b257d5a88cb3794d1bca79f4eb37
Blair <blairacarroll@gmail.com>
2019-08-28 02:12:21 -0400

    Updated components for deleteing

--- ideasboard/src/components/Idea.js
+++ ideasboard/src/components/Idea.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 export default class Idea extends React.Component {
-    handleClick = () => {this.props.onClick(this.props.idea.id)}
+handleClick = () => { this.props.onClick(this.props.idea.id) }
 
-    render(){
-        return(
-            <div className="tile">
-                <h3 onClick={this.handleClick}>{this.props.idea.title}</h3>
-                <p onClick={this.handleClick}>{this.props.idea.body}</p>
-            </div>
-        )
-    }
+	handleDelete = () => { this.props.onDelete(this.props.idea.id) }
+
+	render () {
+		return(
+		  <div className="stickynote">
+		  	<span className="deleteIdea" onClick={this.handleDelete}>x</span>
+		    <h4 onClick={this.handleClick}>{this.props.idea.title}</h4>
+		    <p onClick={this.handleClick}>{this.props.idea.body}</p>
+		  </div>
+		)
+	}
 }
--- ideasboard/src/components/IdeasContainer.js
+++ ideasboard/src/components/IdeasContainer.js
@@ -2,81 +2,80 @@ import React from 'react';
 import axios from 'axios';
 import Idea from './Idea';
 import IdeaForm from './IdeaForm';
+// import Notification from './Notification';
 import update from 'immutability-helper';
 
 export default class IdeasContainer extends React.Component {
-    constructor(props){
-        super(props)
-
-        this.state = {
-            ideas: [],
-            editingIdeaId: null,
-            notification: ''
-        }
+    constructor(props) {
+    super(props)
+    this.state = {
+      ideas: [],
+      editingIdeaId: null,
+      notification: '',
+      transitionIn: false
     }
+  }
 
-    componentDidMount(){
-        axios.get('http://localhost:3001/api/v1/ideas.json')
-        .then(response => {
-            console.log(response)
-            this.setState({ideas: response.data})
-        })
-        .catch(error => console.log(error))
-    }
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/ideas.json')
+    .then(response => {
+      this.setState({ideas: response.data})
+    })
+    .catch(error => console.log(error))
+  }
 
-    addNewIdea = () => {
-        axios.post('http://localhost:3001/api/v1/ideas', {idea: {title: '', body: ''}})
-        .then(response => {
-            const ideas = update(this.state.ideas, { $splice: [[0,0, response.data]]})
-            this.setState({ideas: ideas, editingIdeaId: response.data.id})
-        })
-        .catch(error => console.log(error))
-    }
+  addNewIdea = () => {
+    axios.post('http://localhost:3001/api/v1/ideas', {idea: {title: '', body: ''}})
+    .then(response => {
+      const ideas = update(this.state.ideas, { $splice: [[0, 0, response.data]]})
+      this.setState({ideas: ideas, editingIdeaId: response.data.id})
+    })
+    .catch(error => console.log(error))
+  }
 
-    updateIdea = (idea) => {
-        const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
-        const ideas = update(this.state.ideas, {[ideaIndex]: { $set: idea }})
-        this.setState({ideas: ideas, notification: 'All changes saved'})
-    }
+  updateIdea = (idea) => {
+    const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+    const ideas = update(this.state.ideas, {[ideaIndex]: { $set: idea }})
+    this.setState({ideas: ideas, notification: 'All changes saved', transitionIn: true})
+  }
 
-    resetNotification = () => {this.setState({notification: ''})}
+  deleteIdea = (id) => {
+    axios.delete(`http://localhost:3001/api/v1/ideas/${id}`)
+    .then(response => {
+      const ideaIndex = this.state.ideas.findIndex(x => x.id === id)
+      const ideas = update(this.state.ideas, { $splice: [[ideaIndex, 1]]})
+      this.setState({ideas: ideas})
+    })
+    .catch(error => console.log(error))
+  }
 
-    enableEditing = (id) => {
-        this.setState({editingIdeaId: id})
-    }
+  resetNotification = () => {this.setState({notification: '', transitionIn: false})}
 
-    render(){
-        return(
-            <div>
-                <div>
-                    <button className="newideabutton" onClick={this.addNewIdea}>
-                        New Idea
-                    </button>
-                    <span className="notification">
-                        {this.state.notification}
-                    </span>
-                </div>
-                {this.state.ideas.map((idea) => {
-                    if (this.state.editingIdeaId === idea.id) {
-                        return (
-                            <IdeaForm 
-                            idea={idea} 
-                            key={idea.id} 
-                            updateIdea={this.updateIdea}
-                            resetNotification={this.resetNotification}
-                            />
-                        )
-                    } else {
-                        return (
-                            <Idea 
-                            idea={idea} 
-                            key={idea.id}
-                            onClick={this.enableEditing}
-                            />
-                        )}
-                    }
-                )}
-            </div>
-        )
-    }
+  enableEditing = (id) => {
+    this.setState({editingIdeaId: id}, () => { this.title.focus() })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <button className="newIdeaButton" onClick={this.addNewIdea} >
+            New Idea
+          </button>
+          
+        </div>
+        {this.state.ideas.map((idea) => {
+          if(this.state.editingIdeaId === idea.id) {
+            return(<IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea}
+                    titleRef= {input => this.title = input}
+                    resetNotification={this.resetNotification} />)
+          } else {
+            return (<Idea idea={idea} key={idea.id} onClick={this.enableEditing}
+                    onDelete={this.deleteIdea} />)
+          }
+        })}
+      </div>
+    );
+  }
+
 }
